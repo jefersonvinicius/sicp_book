5ebad99f5746ac26858170d8b76ecf0f5b3a1432
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-07-29 19:41:39 -0300

    Renamed file 1.1.8

--- chapter-01/1.1.8-procedures-as-black-box-abstractions.scm
+++ chapter-01/1.1.8-procedures-as-black-box-abstractions.scm
@@ -1,6 +1,6 @@
 ; The function can defined is used without the programmer know the implementation.
 ; We are not concerned about how the function do your work, we are concerned only the fact 
-;   that it do your work
+;   that it do your work. That's why we can call the function as Black Box
 ; Below, have two function that do the same thing, but with different ways
 
 (define (square x) (* x x))
