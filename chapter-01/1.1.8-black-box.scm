d9912dc5fc5202ecf35802f2b6f6ee6888fd1b1b
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-07-29 19:23:01 -0300

    Add Black box explanation

---
+++ chapter-01/1.1.8-black-box.scm
@@ -0,0 +1,15 @@
+; The function can defined is used without the programmer know the implementation.
+; We are not concerned about how the function do your work, we are concerned only the fact 
+;   that it do your work
+; Below, have two function that do the same thing, but with different ways
+
+(define (square x) (* x x))
+
+(square 5)
+
+(define (square x) 
+  (exp (double (log x))))
+
+(define (double x) (+ x x))
+
+(square 5)
