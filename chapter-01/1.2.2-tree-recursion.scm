4655ed2652c1f506a5abe4e954d79e419e394348
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-07 09:06:55 -0300

    Add fib by iterative way

--- chapter-01/1.2.2-tree-recursion.scm
+++ chapter-01/1.2.2-tree-recursion.scm
@@ -1,4 +1,4 @@
-(define (fib n)
+(define (fib n) ; Recursive
     (cond ((= n 0) 0)
           ((= n 1) 1)
           (else (+ (fib (- n 1))
@@ -7,6 +7,22 @@
           )
     )
 )
+
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+
+(define (fib-iter a b count) ; Iterative
+      (if (= count 0) 
+            b
+            (fib-iter (+ a b) a (- count 1))))
+
+(define (fib n)
+      (fib-iter 1 0 n))
+
 
 (fib 0)
 (fib 1)
